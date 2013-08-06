c38e10d35b2a30c69e77a117e6908de011aed6fb
Manoj Kulkarni <manoj.kulkarni@synechron.com>
2013-08-06 13:08:39 +0530

    Added Timesheet code

---
+++ employee_timesheet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeeTimesheet::Application.config.secret_token = '35ee93887b1c9ee9a8898080140294760e8fdc7f740df6583c3a27187a6968f65dabcf9f3bfeb18e452aa85131c6dd72300467d24ed5f6da43228f10e35f53c2'
