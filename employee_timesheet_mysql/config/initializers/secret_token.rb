5561d4af73c9d8848cae7bd5e3dec68931b7289e
Manoj Kulkarni <manoj.kulkarni@synechron.com>
2013-08-06 15:57:58 +0530

    Project folder structures are added

---
+++ employee_timesheet_mysql/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeeTimesheetMysql::Application.config.secret_token = '99b5d1464765e345c36f619ca3734c9ed139181c0710cca445dc80c2b61c9e1942622b1b7fb3ecbb3b1b50bfb09a8477fe268b541ab9ca85378acad5a3374172'
