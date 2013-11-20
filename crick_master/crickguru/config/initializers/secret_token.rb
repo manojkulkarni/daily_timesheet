dc309d94acc418d0b3525b77fb6c04e771682bbf
Manoj Kulkarni <manoj.kulkarni@synechron.com>
2013-11-20 12:02:08 +0530

    added new folder structure

---
+++ crick_master/crickguru/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crickguru::Application.config.secret_key_base = '372f0c72a9df8b69b25a287d0e1bde4b371fb369087fa079db4863abe0ab8e6255aafea88d9b50993e1a30f6e247eddf2c2d25421bde7f4299f7b26535588d1b'
