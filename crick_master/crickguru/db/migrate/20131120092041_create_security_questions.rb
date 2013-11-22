d14ced66b0f16c322b1b42f5e5515d0d21d7248f
Manoj Kulkarni <manoj.kulkarni@synechron.com>
2013-11-22 13:30:38 +0530

    removed unnecessary migrations

--- crick_master/crickguru/db/migrate/20131120092041_create_security_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSecurityQuestions < ActiveRecord::Migration
-  def change
-    create_table :security_questions do |t|
-      t.string :sec_question
-
-      t.timestamps
-    end
-  end
-end
--- crick_master/crickguru/db/migrate/20131120095719_create_match_squads.rb
+++
@@ -1,8 +0,0 @@
-class CreateMatchSquads < ActiveRecord::Migration
-  def change
-    create_table :match_squads do |t|
-
-      t.timestamps
-    end
-  end
-end
