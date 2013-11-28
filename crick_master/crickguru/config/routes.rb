e17c552235a3d9b0f1e55fba4e6558eac638ee12
Manoj Kulkarni <manoj.kulkarni@synechron.com>
2013-11-28 12:20:50 +0530

    Added sessions controller

--- crick_master/crickguru/config/routes.rb
+++ crick_master/crickguru/config/routes.rb
@@ -1,4 +1,8 @@
 Crickguru::Application.routes.draw do
+  root to: 'sessions#index'
+
+  resources :sessions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
