3031a4338d246a18a4722034de5b85b531548cdf
Manoj Kulkarni <manoj@manoj-Lenovo-G500s.(none)>
2013-12-07 00:13:22 +0530

    Added code to add signup form data

--- crick_master/crickguru/app/controllers/application_controller.rb
+++ crick_master/crickguru/app/controllers/application_controller.rb
@@ -15,7 +15,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+#    devise_parameter_sanitizer.for(:sign_up) << :user_name
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :ph_number, :address, :email, :password, :city, :state, :contry, :PoL, :PoA, :team_name) }
   end
 
   # def configure_permitted_parameters
---
+++ crick_master/crickguru/s/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '58bad3a570dbff9293edc121a89f8504de778aca89635cb145d844e13ca68e98f589c5ceb96fa3b2e2227f40f10161091101772d5421e8396ba7eadc5ffede10';
