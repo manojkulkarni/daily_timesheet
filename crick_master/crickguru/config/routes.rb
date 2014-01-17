6983d7ab71ce374e498ddac3d990ec40dcbb3640
Manoj Kulkarni <manoj.kulkarni@synechron.com>
2014-01-17 15:25:34 +0530

    Added some code

--- crick_master/crickguru/config/routes.rb
+++ crick_master/crickguru/config/routes.rb
@@ -4,6 +4,7 @@ Crickguru::Application.routes.draw do
   resources :players
   resources :teams
   resources :series
+  resources :matches
   # devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
 
